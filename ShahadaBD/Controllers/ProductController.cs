7e5919fa819f765ba9e03460c1a4b170f3a9ab92
Md. Mahfuzullah <mahfuzullah15-11006@diu.edu.bd>
2022-11-06 23:13:33 +0600

    Add project files.

---
+++ ShahadaBD/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShahadaBD.Data;
+using ShahadaBD.Entities;
+
+namespace ShahadaBD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ ShahadaBD/Data/StoreContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShahadaBD.Entities;
+
+namespace ShahadaBD.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base (options)
+        {
+
+        }
+
+        DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+           
+                builder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "Bangladesh"
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "Saudi Arabia"
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Name = "Egypt"
+                    }
+                    );
+            
+        }
+    }
+}
