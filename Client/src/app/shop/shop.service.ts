a234aa7e247a7ac4f871a7798595f9e6909714c6
Md. Mahfuzullah <mahfuzullah15-11006@diu.edu.bd>
2023-01-06 21:26:42 +0600

    Add Searching, Sorting, Filtering & UI.

---
+++ Client/src/app/shop/shop.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { IBrand } from '../shared/models/brand';
+import { IPagination } from '../shared/models/pagination';
+import { IProductType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:7217/api/';
+  shopParams = new ShopParams();
+
+  constructor(private http: HttpClient) { }
+  getProducts(shopParams: ShopParams) {
+
+    let params = new HttpParams();
+
+    if(shopParams.brandId != 0){
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if(shopParams.typeId != 0){
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if(shopParams.search){
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString())
+    params = params.append('pageSize', shopParams.pageSize.toString())
+
+    //console.log("Params", params);
+    
+    return this.http.get<IPagination>(this.baseUrl + 'Products',{observe: 'response', params})
+    .pipe(
+      map(response => {
+        return response.body;
+      })
+    )
+  };
+
+  setShopParams(params: ShopParams) {
+    this.shopParams = params;
+  }
+
+  getShopParams() {
+    return this.shopParams;
+  }
+
+  getBrands(){
+    return this.http.get<IBrand[]>(this.baseUrl + 'Products/Brands');
+  };
+
+  getTypes(){
+    return this.http.get<IProductType[]>(this.baseUrl + 'Products/Types');
+  };
+
+}
--- ShahadaBD/Program.cs
+++ ShahadaBD/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
     });
 });
 
